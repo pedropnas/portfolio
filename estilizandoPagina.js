1a79f5aad62dad15b01614a69bf283bc85f792e1
pedropnas <113207031+pedropnas@users.noreply.github.com>
2022-10-04 11:59:16 -0300

    Add files via upload

---
+++ estilizandoPagina.js
@@ -0,0 +1,40 @@
+function darkMode() {
+    let element = document.body;
+    element.className = "dark-mode"
+}
+
+function lightMode() {
+    let element = document.body;
+    element.className = "light-mode"
+}
+
+function redFontColor() {
+    let element = document.getElementById("main-content");
+    element.className = "red-font"
+}
+
+function blueFontColor() {
+    let element = document.getElementById("main-content");
+    element.className = "blue-font"
+}
+
+function greenFontColor() {
+    let element = document.getElementById("main-content");
+    element.className = "green-font"
+}
+
+
+function font12() {
+    let element = document.getElementById("main-content");
+    element.className = "font12"
+}
+
+function font20() {
+    let element = document.getElementById("main-content");
+    element.className = "font20"
+}
+
+function font36() {
+    let element = document.getElementById("main-content");
+    element.className = "font36"
+}
